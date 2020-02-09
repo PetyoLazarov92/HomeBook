cc90701131df52e5bc6316e0d36e7c723b72e22a
Petyo Lazarov <plazarov92@gmail.com>
2020-02-09 16:35:18 +0200

    Add option to revoke roles from user, in admin panel

--- src/components/admin/AdminPanel.js
+++ src/components/admin/AdminPanel.js
@@ -17,7 +17,6 @@ export default class AdminPanel extends Component {
     getAllUsersAndRoles = () => {
         Promise.all([userManage.loadAllUsers(), userManage.loadUsersRoles()])
             .then(res => {
-                console.log(res)
                 this.setState({
                     allUsers: res[0],
                     allRoles: res[1]
@@ -25,49 +24,10 @@ export default class AdminPanel extends Component {
             })
             .catch(res =>  observer.trigger(observer.events.notification, {type: 'error', message: res.responseJSON.description }));
     }
-    getAllUsers = () => {
-        
-        userManage.loadAllUsers()
-            .then(res => {
-                this.setState({
-                    allUsers: res,
-                    ready: true
-                })
-                console.log(res);
-            })
-            .catch(res =>  observer.trigger(observer.events.notification, {type: 'error', message: res.responseJSON.description }));
-        userManage.loadUsersRoles()
-            .then(res => {
-                // this.setState({
-                //     allUsers: res,
-                //     ready: true
-                // })
-                console.log(res);
-            })
-            .catch(res =>  observer.trigger(observer.events.notification, {type: 'error', message: res.responseJSON.description }));
-        userManage.loadRoleMembers("f21174f8-ef05-4911-a20e-4eb612b2c79c")
-            .then(res => {
-                // this.setState({
-                //     allUsers: res,
-                //     ready: true
-                // })
-                console.log(res);
-            })
-            .catch(res =>  observer.trigger(observer.events.notification, {type: 'error', message: res.responseJSON.description }));
-        userManage.revokeRole("5e3134e20915c73b1875ee6d","f21174f8-ef05-4911-a20e-4eb612b2c79c")
-            .then(res => {
-                // this.setState({
-                //     allUsers: res,
-                //     ready: true
-                // })
-                console.log(res);
-            })
-            .catch(res =>  observer.trigger(observer.events.notification, {type: 'error', message: res.responseJSON.description }));
-    }
     onRoleChange = (event) => {
         console.log(event);
     }
-    onSubmit = (data) => {
+    onSubmitAssign = (data) => {
         if (Object.keys(data).length === 0 && data.constructor === Object) {
             return observer.trigger(observer.events.notification, {type: 'error', message: 'You have not selected an option from the drop-down menu!' })
         }
@@ -77,10 +37,27 @@ export default class AdminPanel extends Component {
         if(!data.role || 'empty' === data.role) {
             return observer.trigger(observer.events.notification, {type: 'error', message: 'You must assign a role to the user!' })
         }
-        console.log(data);
         userManage.assignRole(data.user, data.role)
         .then(res =>{
             observer.trigger(observer.events.notification, {type: 'success', message: "The role was assigned successfully to the user!"})
+            this.props.history.push('/admin-panel');
+        })
+        .catch(res =>  observer.trigger(observer.events.notification, {type: 'error', message: res.responseJSON.description }));
+    }
+    onSubmitRevoke = (data) => {
+        if (Object.keys(data).length === 0 && data.constructor === Object) {
+            return observer.trigger(observer.events.notification, {type: 'error', message: 'You have not selected an option from the drop-down menu!' })
+        }
+        if(!data.user || 'empty' === data.user) {
+            return observer.trigger(observer.events.notification, {type: 'error', message: 'You must select a user!' })
+        }
+        if(!data.role || 'empty' === data.role) {
+            return observer.trigger(observer.events.notification, {type: 'error', message: 'You must to choose a role for removing!' })
+        }
+        console.log(data);
+        userManage.revokeRole(data.user, data.role)
+        .then(res =>{
+            observer.trigger(observer.events.notification, {type: 'success', message: "The role was removed successfully!"})
             this.props.history.push('/admin-panel');
         })
         .catch(res =>  observer.trigger(observer.events.notification, {type: 'error', message: res.responseJSON.description }));
@@ -119,7 +96,7 @@ export default class AdminPanel extends Component {
                 </div>
                 <div className="col-6 pr-0">
                     <hr className="mt-0"/>
-                    <BoundForm onSubmit={this.onSubmit} className="form-horizontal">
+                    <BoundForm onSubmit={this.onSubmitAssign} className="form-horizontal">
                         <label htmlFor="user">User:</label>
                         <select name='user' className='form-control'>
                             <option value="empty">- Select user -</option>
@@ -133,6 +110,25 @@ export default class AdminPanel extends Component {
                         </select>
 
                         <input type='submit' className="btn btn-success mt-2 mb-2" value='Assign'/>
+                    </BoundForm>
+
+                    <h2 className="title d-inline-block mt-4">Revoke role from user</h2>
+                    <hr/>
+
+                    <BoundForm onSubmit={this.onSubmitRevoke} className="form-horizontal">
+                        <label htmlFor="user">User:</label>
+                        <select name='user' className='form-control'>
+                            <option value="empty">- Select user -</option>
+                            {this.state.allUsers.map((u, i) => <option key={u._id} value={u._id}>{u.username}</option>)}
+                        </select>
+
+                        <label htmlFor="role">Assign role:</label>
+                        <select name='role' className='form-control'>
+                            <option value="empty">- Select role -</option>
+                            {this.state.allRoles.map((r, i) => <option key={r._id} value={r._id}>{r.name}</option>)}
+                        </select>
+
+                        <input type='submit' className="btn btn-danger mt-2 mb-2" value='Revoke'/>
                     </BoundForm>
                 </div>
             </div>
